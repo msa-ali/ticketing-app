7725d837ba80f8f8eec2444057ed2d0747a40cf8
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-02-10 22:50:11 +0530

    expiration service setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,30 @@
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS CLIENT ID MUST BE DEFINED');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS URL MUST BE DEFINED');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS Cluster ID MUST BE DEFINED');
+  }
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS Connection Close');
+      process.exit();
+    })
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+start();
